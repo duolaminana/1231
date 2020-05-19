28905be37806935bdac35cdc5a27492d81a4a275
leina <675549518@qq.com>
2020-05-19 10:04:13 +0800

    'tabbar'

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // module: {
+  //   rules: {
+  //     test: /\.sass$/,
+  //     loaders: ['style', 'css', 'sass'],
+  //   },
+  // },
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       assets: '@/assets',
+  //       common: '@/common',
+  //       components: '@/components',
+  //       api: '@/api',
+  //       views: '@/views',
+  //     },
+  //   },
+  // },
+}
